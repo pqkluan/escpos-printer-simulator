f87eb8a34fded134ad8643ced09812c779fb9bc8
Phạm Quan Khiết Luân <pqkluan@gmail.com>
2018-05-24 17:55:03 +0700

    Update main app for better logging

--- src/biz/iteksolutions/PrinterServer.java
+++ src/biz/iteksolutions/PrinterServer.java
@@ -7,78 +7,75 @@ import java.net.Socket;
 
 public class PrinterServer extends Thread {
 
-    private IPrinterOutput display;
-    private Socket socket;
-    private int clientNumber;
+		private IPrinterOutput display;
+		private Socket socket;
+		private int clientNumber;
 
-    public PrinterServer(IPrinterOutput display, Socket socket, int clientNumber) {
-        this.display = display;
-        this.socket = socket;
-        this.clientNumber = clientNumber;
-        log("New connection with client# " + clientNumber + " at " + socket);
-    }
+		// Constructor
+		public PrinterServer(IPrinterOutput display, Socket socket, int clientNumber) {
+				this.display = display;
+				this.socket = socket;
+				this.clientNumber = clientNumber;
+				log("Socket no." + clientNumber + " opened");
+		}
 
-    @Override
-    public void run() {
-        try {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(), "GBK")); //GBK charset is for textChinese support
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		@Override
+		public void run() {
+				try {
+						BufferedReader in = new BufferedReader(
+										new InputStreamReader(socket.getInputStream(), "GBK")); // GBK charset is for textChinese support
 
-            // Send a welcome message to the client.
-            //out.println("Hello, you are client #" + clientNumber + ".");
-            //out.println("Enter a line with only a period to quit\n");
+						// Get messages from the client, line by line
+						// And send these messages to the output
+						String sCurrentLine;
+						while ((sCurrentLine = in.readLine()) != null) {
+								log(sCurrentLine);
 
-            // Get messages from the client, line by line; return them
-            String sCurrentLine;
-            while ((sCurrentLine = in.readLine()) != null) {
-                //out.println(sCurrentLine);
-                Printer po = new Printer();
-                for (int i = 0; i < sCurrentLine.length(); i++) {
-                    //System.out.println(i + ": " + input.charAt(i));
-                    po.addChar(sCurrentLine.charAt(i));
-                }
+								Printer printer = new Printer();
+								for (int i = 0; i < sCurrentLine.length(); i++) {
+										// System.out.println(i + ": " + input.charAt(i));
+										printer.addChar(sCurrentLine.charAt(i));
+								}
 
-                //Explicitly add new line here because in.readLine ignore LF
-                po.getCommands().add(new LineFeedCommand());
+								// Explicitly add new line here because in.readLine ignore LF
+								printer.getCommands().add(new LineFeedCommand());
 
-                //handle QR print - QR command content is 4th
-                int qrCount = 0;
-                for (Command cmd : po.getCommands()) {
-                    if (cmd instanceof PrintQRCommand) {
-                        qrCount++;
-                        if (qrCount == 4) {
-                            ((PrintQRCommand) cmd).setContentFlag(true);
-                        }
-                        if (qrCount == 5) {
-                            //reset value
-                            qrCount = 0;
-                        }
-                    }
-                }
+								// handle QR print - QR command content is 4th
+								int qrCount = 0;
+								for (Command cmd : printer.getCommands()) {
+										if (cmd instanceof PrintQRCommand) {
+												qrCount++;
 
-                if (po.getCommands().size() > 0) {
-                    for (Command obj : po.getCommands()) {
-                        if (obj instanceof IContentOutput) {
-                            IContentOutput container = (IContentOutput) obj;
-                            display.setText(container.getText());
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            log("Error handling client# " + clientNumber + ": " + e);
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                log("Couldn't close a socket, what's going on?");
-            }
-            log("Connection with client# " + clientNumber + " closed");
-        }
-    }
+												if (qrCount == 4)
+														((PrintQRCommand) cmd).setContentFlag(true);
 
-    private void log(String message) {
-        System.out.println(message);
-    }
+												if (qrCount == 5)
+														qrCount = 0; // reset value
+										}
+								}
+
+								if (printer.getCommands().size() > 0) {
+										for (Command obj : printer.getCommands()) {
+												if (obj instanceof IContentOutput) {
+														IContentOutput container = (IContentOutput) obj;
+														display.setText(container.getText());
+												}
+										}
+								}
+						}
+				} catch (IOException e) {
+						log("Error handling client# " + clientNumber + ": " + e);
+				} finally {
+						try {
+								socket.close();
+						} catch (IOException e) {
+								log("Couldn't close a socket, what's going on?");
+						}
+						log("Socket no." + clientNumber + " closed");
+				}
+		}
+
+		private void log(String message) {
+				System.out.println(message);
+		}
 }
--- src/biz/iteksolutions/Program.java
+++ src/biz/iteksolutions/Program.java
@@ -6,53 +6,58 @@ import java.net.ServerSocket;
 
 public class Program {
 
-    /**
-     *
-     * @param args
-     * args[0] - port - require if use file in args[1]
-     * args[1] - optional - file path - output to file only
-     * args[2] - optional - file size KB
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        int clientNumber = 0;
-        int port = 9100;
-        String path = "";
-        int size = 200;
-        IPrinterOutput output = null;
+		private static final int DEFAULT_PORT = 9100;
+		private static final String DEFAULT_PATH = "";
+		private static final int DEFAULT_FILE_SIZE = 200;
 
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        }
+		private static IPrinterOutput createPrinterOutput(boolean isGUI, String outputFilePath, int outputFileSize) {
+				// Write the output to file
+				if (!isGUI)
+						return new FileOutputImpl(outputFilePath, outputFileSize);
 
-        ServerSocket listener = new ServerSocket(port);
+				// Display GUI
+				PrinterOutputImpl pPanel = new PrinterOutputImpl();
+				JFrame frame = new JFrame("ESC/POS Printer Simulator");
+				frame.setContentPane(pPanel);
+				frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+				frame.pack();
+				frame.setVisible(true);
+				return pPanel;
+		}
 
-        if (args.length > 1) {
-            path = args[1];
-        }
-        if (args.length > 2) {
-            size = Integer.parseInt(args[2]);
-        }
+		/**
+		 * Main app
+		 * 
+		 * @param args
+		 *        args[0] - port - require if use file in args[1] 
+		 *        args[1] - optional - file path - output to file only 
+		 *        args[2] - optional - file size KB
+		 * @throws IOException
+		 */
+		public static void main(String[] args) throws IOException {
+				// The number of times this printer was connected
+				int clientNumber = 1;
 
-        if (path.isEmpty()) {
-            PrinterOutputImpl pPanel = new PrinterOutputImpl();
-            JFrame frame = new JFrame("ESC/POS Printer Simulator - Port " + port);
-            frame.setContentPane(pPanel);
-            frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
-            frame.pack();
-            frame.setVisible(true);
-            output = pPanel;
-        } else {
-            output = new FileOutputImpl(path, size);
-        }
+				// Params pass when run the jar file
+				int port = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
+				String path = args.length > 1 ? path = args[1] : DEFAULT_PATH;
+				int fileSize = args.length > 2 ? Integer.parseInt(args[2]) : DEFAULT_FILE_SIZE;
 
-        try {
-            while (true) {
-                PrinterServer printerServer = new PrinterServer(output, listener.accept(), clientNumber++);
-                printerServer.start();
-            }
-        } finally {
-            listener.close();
-        }
-    }
+				IPrinterOutput output = createPrinterOutput(path.isEmpty(), path, fileSize);
+				ServerSocket listener = new ServerSocket(port); // Pointer for closing port listener
+
+				System.out.println("Print Emulator start with these params");
+				System.out.println("SOCKET PORT: " + port);
+				System.out.println("OUTPUT FILE PATH: " + path);
+				System.out.println("OUTPUT FILE SIZE: " + fileSize);
+
+				try {
+						while (true) {
+								PrinterServer printerServer = new PrinterServer(output, listener.accept(), clientNumber++);
+								printerServer.start();
+						}
+				} finally {
+						listener.close();
+				}
+		}
 }
